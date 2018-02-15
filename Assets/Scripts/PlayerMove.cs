d95ce67881047e409b2925334240f97f6a334cfe
darrrrUC <ema_carlsson@yahoo.com>
2018-02-15 14:47:32 +0100

    init

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+
+    public float speed;
+    private Camera cam;
+    public Material playerSkin;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (isLocalPlayer)
+        {
+            
+            return;
+        }
+
+        cam.enabled = false;
+        
+
+    }
+
+    void FixedUpdate()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+        var tmp = cam.transform.position = this.transform.position;
+        tmp.y = this.transform.position.y +5;
+        tmp.z = this.transform.position.z -10;
+        cam.transform.position = tmp;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+
+    public override void OnStartLocalPlayer()
+    {
+        cam = Camera.main;
+
+        var render = GetComponent<Renderer>();
+        render.material = playerSkin;
+    }
+}
