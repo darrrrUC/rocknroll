4a400b56b4090924693b910884b08a27c147e922
darrrrUC <ema_carlsson@yahoo.com>
2018-02-19 18:50:33 +0100

    score increment

--- Assets/Scripts/RegisterMatch.cs
+++ Assets/Scripts/RegisterMatch.cs
@@ -5,14 +5,34 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.Networking.Match;
+using UnityEngine.UI;
 
 public class RegisterMatch : NetworkBehaviour {
 
     public Guid playerId;
+    public List<Text> scoreText; 
     private bool isMatchRegistred;
+    private Guid gameId;
+    private int score;
+
+    //server specific:
+    private List<GameObject> players = new List<GameObject>();
+    private List<int> scores = new List<int>();
+    private List<Guid> playerIds = new List<Guid>();
+
+    //temp increment score for testing only
+    private int tempScore = 1;
+
     void Start()
     {
         playerId = Guid.NewGuid();
+        var scoreObjects = GameObject.FindGameObjectsWithTag("ScoreText");
+        foreach (var item in scoreObjects)
+        {
+            scoreText.Add(item.GetComponent<Text>());
+        }
+        scoreText.Reverse();
+      
 
     }
 
@@ -24,19 +44,19 @@ public class RegisterMatch : NetworkBehaviour {
     void Update()
     {
 
-        if (!isServer)
-        {
-            return;
-        }
-        if (!isLocalPlayer)
+
+
+
+        if (!isServer || !isLocalPlayer)
         {
             return;
         }
 
+
         if (Input.GetKeyDown(KeyCode.P) && !isMatchRegistred)
         {
-            List<GameObject> players = new List<GameObject>();
-            List<Guid> playerIds = new List<Guid>();
+            
+            
             foreach (var player in GameObject.FindGameObjectsWithTag("Player").Distinct().ToList())
             {
                 players.Add(player);
@@ -48,20 +68,47 @@ public class RegisterMatch : NetworkBehaviour {
             }
             StartCoroutine(PostMatch(playerIds));
             isMatchRegistred = true;
-
-            
-
         }
-    
+
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            StartCoroutine(PostScore(playerIds[0], tempScore));
+            tempScore += 1;
+        }
+
+
+
+
+        //if (Input.GetKeyDown(KeyCode.L))
+        //{
+        //    StartCoroutine(GetPlayers());
+        //}
+
     }
 
+    IEnumerator PostScore(Guid playerId, int score)
+    {
+        WWWForm form = new WWWForm();
+        var download = UnityWebRequest.Post("http://localhost:60728/api/Score/PostScore?playerId=" + playerId.ToString() + "&score=" + score.ToString(), form);
 
+        yield return download.SendWebRequest();
+
+        if (download.isNetworkError || download.isHttpError)
+        {
+            Debug.Log("Shit has hit the fan when posting game info");
+        }
+        else
+        {
+            Debug.Log("Successfully posted game score");
+        }
+    }
  
     IEnumerator PostMatch(List<Guid> playerIds)
     {
         Guid p1 = new Guid(), p2 = new Guid(), p3 = new Guid(), p4 = new Guid(), p5 = new Guid(), p6 = new Guid(), p7 = new Guid(), p8 = new Guid();
-        Guid gameId = Guid.NewGuid();
-        WWWForm form = new WWWForm();
+        gameId = Guid.NewGuid();
+      
 
         if (playerIds.Count == 1)
         {
@@ -126,20 +173,38 @@ public class RegisterMatch : NetworkBehaviour {
 
 
 
-
+        WWWForm form = new WWWForm();
         var download = UnityWebRequest.Post("http://localhost:60728/api/Score/RegisterNewGame" + "?GameId=" + gameId + "&Player1=" + p1 + "&Player2=" + p2 + "&Player3=" + p3 + "&Player4=" + p4 + "&Player5=" + p5 + "&Player6=" + p6 + "&Player7=" + p7 + "&Player8=" + p8, form);
 
         yield return download.SendWebRequest();
 
         if (download.isNetworkError || download.isHttpError)
         {
-            Debug.Log("Shit has hit the fan");
+            Debug.Log("Shit has hit the fan when posting game info");
         }
         else
         {
-            Debug.Log("Successfully posted score");
+            Debug.Log("Successfully posted game info");
         }
 
 
+    }
+
+    IEnumerator GetPlayers()
+    {
+
+        WWWForm form = new WWWForm();
+        var download = UnityWebRequest.Get("http://localhost:60728/api/Score/GetPlayersFromGame?GameId=" + gameId);
+
+        yield return download.SendWebRequest();
+
+        if (download.isNetworkError || download.isHttpError)
+        {
+            Debug.Log("Shit has hit the fan when fetching players");
+        }
+        else
+        {
+            Debug.Log("Successfully fetched players");
+        }
     }
 }
